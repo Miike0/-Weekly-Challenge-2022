ddfbb3c19cd486bb015f3c75770d3c73f405a275
Miikeo <Miguel.A.INCO@gmail.com>
2022-11-02 20:32:02 -0600

    Challenge 18 completed successfully

---
+++ challenges/challenge18.js
@@ -0,0 +1,34 @@
+/*
+ * Reto #19
+ * CONVERSOR TIEMPO
+ * Fecha publicación enunciado: 09/05/22
+ * Fecha publicación resolución: 16/05/22
+ * Dificultad: FACIL
+ *
+ * Enunciado: Crea una función que reciba días, horas, minutos y segundos (como enteros) y retorne su resultado en milisegundos.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+function timeToMiliseconds (days, hours, minutes, seconds) {
+    let time = {
+        second: 1000,
+        minute: 1000 * 60,
+        hour: 1000 * 60 * 60,
+        day: 1000 * 60 * 60 * 24,
+    }
+    
+    return (days * time.day) + (hours * time.hour) + (minutes * time.minute) + (seconds * time.second);
+}
+
+function main () {
+    console.log(timeToMiliseconds(0,0,0,10));
+    console.log(timeToMiliseconds(2,5,-45,10));
+    console.log(timeToMiliseconds(2000000000,5,45,10));
+}
+main();
